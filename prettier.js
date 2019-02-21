22dd31cc93528ff9d29d162fb39c10a77d886ba3
Tyom Semonov <inspiro@gmail.com>
2019-02-21 22:06:12 +0000

    Remove plugins property in prettier config
    It is loaded in Prettier plugin config

--- prettier.js
+++ prettier.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: ['./base.js', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'all' }],
   },
